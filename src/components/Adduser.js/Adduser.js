b96a19a3c39388cfd429b13e29359c43432a3ad6
Masoumeh Rezai <you@example.com>
2023-07-14 07:27:04 +0430

    last commit

--- src/components/Adduser.js/Adduser.js
+++ src/components/Adduser.js/Adduser.js
@@ -1,4 +1,4 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import Card from "../Card/Card";
 import classes from './Adduser.module.css'
 import Button from "../Button/Button";
@@ -6,20 +6,43 @@ import Head from '../Head/Head'
 import Footer from "../Footer/Footer";
 
 const Adduser = () => {
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+    const formHandler = (event) => {
+        event.preventDefault();
+        if(name.trim()==='' || password.trim()===''){
+            return alert("can submit form correct")
+        }
+        console.log(name);
+        console.log(password);
+        setName('');
+        setPassword('');
+    }
+    const inputNameHanlder = (event) => {
+        setName(event.target.value);
+
+
+    }
+    const inputPasswordHanlder = (event) => {
+        setPassword(event.target.value);
+
+    }
+
+
     return (
         <Fragment>
             <Card>
-            <Head/>
+                <Head />
 
-                <form className={classes.form} >
-                
+                <form className={classes.form} onSubmit={formHandler}>
+
                     <label className={classes.label}>UserName or Email</label>
-                    <input className={classes.input}></input>
+                    <input className={classes.input} onChange={inputNameHanlder} value={name}></input>
                     <label className={classes.label}>Password</label>
-                    <input className={classes.input}></input>
+                    <input className={classes.input} onChange={inputPasswordHanlder} value={password}></input>
                     <Button>Login</Button>
                 </form>
-                <Footer/>
+                <Footer />
             </Card>
 
         </Fragment>
