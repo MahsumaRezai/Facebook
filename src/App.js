2e516635f5fb43b111e6fe3ebc10993851400616
Masoumeh Rezai <you@example.com>
2023-07-14 05:45:09 +0430

    add head section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { Fragment } from "react";
 import Adduser from "./components/Adduser.js/Adduser";
-
+import Head from './components/Head/Head'
 function App() {
   return (
     <Fragment>
+      <Head />
       <Adduser />
     </Fragment>
 
---
+++ src/components/Head/Head.js
@@ -0,0 +1,14 @@
+import { Fragment } from "react"
+
+const Head = () => {
+    return (
+        <Fragment>
+            <div>
+                <h1>Facebook</h1>
+
+            </div>
+        </Fragment>
+    )
+
+}
+export default Head;
