1512f57610b07dcbacf59fb2cb378168d3f54ceb
Masoumeh Rezai <you@example.com>
2023-07-14 05:22:17 +0430

    add input section

--- src/components/Adduser.js/Adduser.js
+++ src/components/Adduser.js/Adduser.js
@@ -1,13 +1,17 @@
 import { Fragment } from "react";
 import Card from "../Card/Card";
+import classes from './Adduser.module.css'
 
 const Adduser = () => {
     return (
         <Fragment>
             <Card>
-                <form >
-                    <input placeholder="User / Email"></input>
-                    <input placeholder="Password"></input>
+                <form className={classes.form} >
+                
+                    <label className={classes.label}>UserName or Email</label>
+                    <input className={classes.input}></input>
+                    <label className={classes.label}>Password</label>
+                    <input className={classes.input}></input>
                     <button></button>
                 </form>
             </Card>
